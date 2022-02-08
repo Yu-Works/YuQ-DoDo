4c7a2b8f6d5dc54dc4c172d5bfd0e0e974123ead
IceCream <www@withdata.net>
2022-02-09 04:38:01 +0800

    实现 YuQ。

--- src/main/kotlin/com/icecreamqaq/yuq/dodo/DoDo.kt
+++ src/main/kotlin/com/icecreamqaq/yuq/dodo/DoDo.kt
@@ -2,15 +2,16 @@ package com.icecreamqaq.yuq.dodo
 
 import com.IceCreamQAQ.Yu.`as`.ApplicationService
 import com.IceCreamQAQ.Yu.annotation.Config
+import com.IceCreamQAQ.Yu.di.YuContext
+import com.IceCreamQAQ.Yu.event.EventBus
 import com.IceCreamQAQ.Yu.util.Web
-import com.icecreamqaq.yuq.GuildList
-import com.icecreamqaq.yuq.YuQ
-import com.icecreamqaq.yuq.YuQVersion
+import com.icecreamqaq.yuq.*
+import com.icecreamqaq.yuq.dodo.entity.GuildImpl
 import com.icecreamqaq.yuq.entity.*
 import com.icecreamqaq.yuq.message.MessageItemFactory
 import javax.inject.Inject
 
-class DoDo : ApplicationService, YuQVersion, YuQ {
+class DoDo : ApplicationService, YuQVersion, YuQ, User {
 
     @Config("YuQ.DoDo.clientId")
     lateinit var clientId: String
@@ -26,29 +27,56 @@ class DoDo : ApplicationService, YuQVersion, YuQ {
 
     lateinit var op: OpDoDo
 
+    @Inject
+    override lateinit var messageItemFactory: MessageItemFactory
+
+    @Inject
+    override lateinit var web: Web
+
+    @Inject
+    lateinit var internalBot: YuQInternalBotImpl
+
+    @Inject
+    lateinit var context: YuContext
+
+    @Inject
+    lateinit var eventBus:EventBus
+
+
     override fun init() {
         op = OpDoDo(clientId, token, publicKey, clientSecret)
 
+        dodo = this
+        opDoDo = op
+        mif = messageItemFactory
+        com.icecreamqaq.yuq.eventBus = eventBus
+
         op.run {
-            println(botInfo())
-//            println(guildList())
+            botInfo().let {
+                id = it.dodoId
+                name = it.nickName
+                avatar = it.avatarUrl
+            }
             println(guildInfo(199899))
-            println(channelList(199899))
-            val id = sendMessageToChannel(177531, 1, "测试消息1").messageId
-            println(
-                sendMessageToChannel(
-                    177531,
-                    1,
-                    "**测试***消*[息](https://www.baidu.com/)2__456__~~测试~~||防剧透||\n>123456\n1`23`4\n1```23456```6"
-                )
-            )
-            println(recallMessage(id))
-            ws()
         }
+//        refreshGuilds()
     }
 
+    private fun getOrNewGuild(id: Long): GuildImpl =
+        guilds.getOrPut(id) {
+            opDoDo.guildInfo(id).let {
+                GuildImpl(
+                    id,
+                    it.isLandName,
+                    it.coverUrl
+                )
+            }
+        }
+
+
     override fun start() {
-//        op.ws()
+        context.injectBean(internalBot)
+        op.ws()
     }
 
     override fun stop() {
@@ -60,26 +88,47 @@ class DoDo : ApplicationService, YuQVersion, YuQ {
     override fun runtimeName() = "YuQ-DoDo"
     override fun runtimeVersion() = "0.1.0-DEV"
 
-    override val botId: Long
-        get() = clientId.toLong()
-    override val botInfo: User
-        get() = TODO("Not yet implemented")
-    override val cookieEx: YuQ.QQCookie
-        get() = TODO("Not yet implemented")
-    override val friends = UserListImpl<Friend>()
-    override val groups = UserListImpl<Group>()
-    override var guilds = UserListImpl<Guild>()
-    override val messageItemFactory: MessageItemFactory
-        get() = TODO("Not yet implemented")
 
-    @Inject
-    override lateinit var web: Web
+    override var guilds = UserListImpl<GuildImpl>()
+
+    override fun refreshGuilds(): GuildList {
+        val gs = op.guildList()
+        val ngs = UserListImpl<GuildImpl>()
+        gs.forEach {
+            ngs.add(
+                guilds[it.islandId]?.apply {
+                    if (name != it.isLandName) name = it.isLandName
+                } ?: GuildImpl(it.islandId, it.isLandName, it.coverUrl)
+            )
+        }
+        guilds = ngs
+        return guilds
+    }
+
+    override val botId: Long
+        get() = id
+    override val botInfo: User
+        get() = this
+
+    override var id: Long = 0
+    override val platformId: String
+        get() = id.toString()
+
+    override var name = ""
+    override var avatar: String = ""
+
+    override fun canSendMessage() = false
+    override fun isFriend() = false
 
     override fun id2platformId(id: Long) = id.toString()
     override fun platformId2id(platformId: String) = platformId.toLong()
 
+    override val friends = UserListImpl<Friend>()
+    override val groups = UserListImpl<Group>()
     override fun refreshFriends() = friends
     override fun refreshGroups() = groups
 
+    override val cookieEx: YuQ.QQCookie
+        get() = TODO("Not yet implemented")
 
 }
